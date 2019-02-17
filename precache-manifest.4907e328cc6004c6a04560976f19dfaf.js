e1cd8e41502722643b823e10e68adc37804cd520
shanhinpavel <shanmain@tut.by>
2019-02-17 21:09:50 +0300

    Updates

---
+++ precache-manifest.4907e328cc6004c6a04560976f19dfaf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3fb3b12704e78f194a7",
+    "url": "/mentors-dashboard/static/js/runtime~main.f3fb3b12.js"
+  },
+  {
+    "revision": "919bf19420c21bd5da1a",
+    "url": "/mentors-dashboard/static/js/main.919bf194.chunk.js"
+  },
+  {
+    "revision": "af1c81f3d582c91c9872",
+    "url": "/mentors-dashboard/static/js/2.af1c81f3.chunk.js"
+  },
+  {
+    "revision": "fbcb4a6bd5c9c5f5de86ccd5253c5daf",
+    "url": "/mentors-dashboard/index.html"
+  }
+];
