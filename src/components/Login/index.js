2fd10ceed8130e18ef504132fa7a67aefaee3653
Pavel Shanhin <shanMail@tut.by>
2019-02-19 17:35:36 +0300

    fix: change url

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -11,9 +11,17 @@ function LoginButton(props) {
     buttonTitle = 'Logout';
   }
 
+  const key = {
+    clientID: '0aef0ba45b9e01f7c40e',
+    clientSecret: 'f940de19a47c1d1e28418700e304851121fe7864',
+  };
+
+  const redirectUrl = 'https://shanhinpavel.github.io/mentors-dashboard/';
+  const authUrl = `http://github.com/login/oauth/authorize?client_id=${key.clientID}redirect_uri=${redirectUrl}`;
+
   return (
     <div style={buttonStyle}>
-      <a href="https://github.com/login/oauth/authorize" style={{ textDecoration: 'none' }}>{buttonTitle}</a>
+      <a href={authUrl} style={{ textDecoration: 'none' }}>{buttonTitle}</a>
     </div>
   );
 }
