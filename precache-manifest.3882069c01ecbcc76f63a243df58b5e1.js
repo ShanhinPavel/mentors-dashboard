8ee9110b48de48b296bfed4714935b9c0a088074
Pavel Shanhin <shanMail@tut.by>
2019-02-23 19:36:22 +0300

    Updates

---
+++ precache-manifest.3882069c01ecbcc76f63a243df58b5e1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3fb3b12704e78f194a7",
+    "url": "/mentors-dashboard/static/js/runtime~main.f3fb3b12.js"
+  },
+  {
+    "revision": "fcb50d6c120a32559ef2",
+    "url": "/mentors-dashboard/static/js/main.fcb50d6c.chunk.js"
+  },
+  {
+    "revision": "0793cbed584305db8420",
+    "url": "/mentors-dashboard/static/js/2.0793cbed.chunk.js"
+  },
+  {
+    "revision": "9d58ec9cf7792d634d133b3cff5c9f23",
+    "url": "/mentors-dashboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mentors-dashboard/precache-manifest.af48fab9e490eacbadfd7377dcc167b8.js"
+  "/mentors-dashboard/precache-manifest.3882069c01ecbcc76f63a243df58b5e1.js"
 );
 
 workbox.clientsClaim();
